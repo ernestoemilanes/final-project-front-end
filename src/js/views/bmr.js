0a41066af968fafa31b2cc6382af9eb4e949e4b6
ernestoemilanes <ernestoemilanes@gmail.com>
2021-01-05 21:13:37 +0000

    css changes

--- src/js/views/bmr.js
+++ src/js/views/bmr.js
@@ -15,48 +15,48 @@ export const Bmr = () => {
 			</div>
 			<h1 className="text-center text-light pt-3">BMR Calculator</h1>
 			<div id="block" className="row justify-content-center text-center pt-3">
-				<div className="col-md-3 text-danger">
+				<div className="col-md-3 text-warning">
 					<h4>Weight</h4>
 					<input id="weight" type="text" placeholder="lbs" onChange={e => setWeight(e.target.value)} />
 				</div>
-				<div className="col-md-3 text-danger">
+				<div className="col-md-3 text-warning">
 					<h4>Height</h4>
 					<input id="height" type="text" placeholder="inches" onChange={e => setHeight(e.target.value)} />
 				</div>
-				<div className="col-md-3 text-danger">
+				<div className="col-md-3 text-warning">
 					<h4>Age</h4>
 					<input id="age" type="text" onChange={e => setAge(e.target.value)} />
 				</div>
-				<div className="col-md-3 text-danger">
-					<form>
-						<input
-							type="radio"
-							id="male"
-							name="gender"
-							value="male"
-							onChange={e => setGender(e.target.value)}
-						/>
-						<label>Male</label>
-						<input
-							type="radio"
-							id="female"
-							name="gender"
-							value="female"
-							onChange={e => setGender(e.target.value)}
-						/>
-						<label>Female</label>
-					</form>
-				</div>
+			</div>
+			<div className="text-center text-warning pt-5">
+				<form>
+					<input
+						type="radio"
+						id="male"
+						name="gender"
+						value="male"
+						onChange={e => setGender(e.target.value)}
+					/>
+					<label className="pr-3">Male</label>
+					<input
+						type="radio"
+						id="female"
+						name="gender"
+						value="female"
+						onChange={e => setGender(e.target.value)}
+					/>
+					<label>Female</label>
+				</form>
 			</div>
 			<div className="text-center pt-5">
 				<button
 					id="bmrButton"
-					className="btn btn-danger"
+					className="btn btn-warning"
 					onClick={() => actions.bmrResult(weight, height, age, gender)}>
 					Click for BMR
 				</button>
 			</div>
-			<div className="text-center text-light pt-5">
+			<div className="text-center text-warning pt-5">
 				<h1>{store.bmr}</h1>
 			</div>
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -66,10 +66,10 @@ export const Home = () => (
 		{/* Jumbotron */}
 		<div className="container text-center">
 			<div className="row">
-				<div className="col-md-7 col-sm-12">
+				<div className="col-md-7 col-sm-12 text-light">
 					<h1>“We are what we repeatedly do. Excellence then is not an act but a habit.”</h1>
 					{/* <h6>ARISOTLE</h6> */}
-					<button className="btn btn-dark">GET STARTED</button>
+					<button className="btn btn-warning">GET STARTED</button>
 				</div>
 				<div className="col-md-5 col-sm-12 h-15">
 					<img src={manRunning} alt="Running" />
