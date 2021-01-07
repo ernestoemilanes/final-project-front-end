835107b8243ea96de75f5e9e9957c5a7868ad2b8
ernestoemilanes <ernestoemilanes@gmail.com>
2021-01-07 04:42:22 +0000

    clean up

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,23 +8,17 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 	return (
 		<>
-			{/* Container Fluid */}
 			<Navigation />
-			{/* End of Container Fluid */}
-			{/* Jumbotron */}
-			<div className="container text-center">
+			{/* <div className="container text-center">
 				<div className="row">
 					<div className="col-md-7 col-sm-12 text-white">
 						<h1>“We are what we repeatedly do. Excellence then is not an act but a habit.”</h1>
-						{/* <h6>ARISOTLE</h6> */}
-						{/* <button className="btn btn-warning">GET STARTED</button> */}
 					</div>
 					<div className="col-md-5 col-sm-12">
 						<img src={manRunning} alt="Running" />
 					</div>
 				</div>
-			</div>
-			{/* End of Jumbotron */}
+			</div> */}
 		</>
 	);
 };
